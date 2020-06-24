2f45eb07cd9d608c4e2a40507743f730ee70e192
Allene Norton <norton.allene@gmail.com>
2020-06-24 20:01:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  current_min = name_hash[0,0]
+  current_min = name_hash[0]
   name_hash.each do |k,v|
     if v < current_min
       current_min = k,v
