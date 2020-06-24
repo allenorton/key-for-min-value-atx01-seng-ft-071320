93aa8b188976788a664c39946037b7dc84337e27
Allene Norton <norton.allene@gmail.com>
2020-06-24 20:00:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   current_min = name_hash[0,0]
   name_hash.each do |k,v|
     if v < current_min
-      current_min = v
+      current_min = k,v
     end 
     p k v
   end
